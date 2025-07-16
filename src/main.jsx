3bcb56d35dbe3d71287d8b9f96653b03baabb1b3
shohagnawaz <snawazdigital@gmail.com>
2025-07-16 18:44:10 +0600

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import {
+  RouterProvider
+} from "react-router-dom";
+import { router } from './Routes/router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className='max-w-7xl mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 )
