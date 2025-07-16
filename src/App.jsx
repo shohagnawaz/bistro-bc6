5e64fa5323ca4f9b8d34ecdfa6398db7b5bdbd75
shohagnawaz <snawazdigital@gmail.com>
2025-07-16 17:33:02 +0600

    bistro

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='red'>Md. Shah Nawaz Shohag</h2>
+      <button></button>
+      <div className="card card-border bg-base-100 w-96">
+  <div className="card-body">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
